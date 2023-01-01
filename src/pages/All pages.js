367ccfd55e21fe62cdb1a2e4ecbb2b4958349b0d
Bjarki Ágúst Guðmundsson <bjarki.agust@gmail.com>
2023-01-01 20:54:47 +0100

    Implement basic page overview

---
+++ src/pages/All pages.js
@@ -0,0 +1,36 @@
+import * as React from "react"
+import { graphql, Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const AllPages = ({ data }) => (
+  <Layout>
+    <h1>All pages</h1>
+    <ul>
+      {data.pages.nodes.map(page => (
+        <li key={page.parent.name}>
+          <Link to={`/${page.parent.name}`}>{page.parent.name}</Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+)
+
+export const query = graphql`
+  query Pages {
+    pages: allMarkdownRemark {
+      nodes {
+        parent {
+          ... on File {
+            name
+          }
+        }
+      }
+    }
+  }
+`
+
+export const Head = () => <Seo title="All pages" />
+
+export default AllPages
