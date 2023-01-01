85b9ab79f80cc5a8f011c7143c3e5606ec550618
Bjarki Ágúst Guðmundsson <bjarki.agust@gmail.com>
2023-01-01 16:18:12 +0100

    Remove boilerplate

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,10 +9,4 @@
  */
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
 }
