0fc283e92099e1e57a7f11b5f88dc09d8a40921e
Bjarki Ágúst Guðmundsson <bjarki.agust@gmail.com>
2023-01-01 20:55:46 +0100

    Create page paths

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,42 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
+const path = require("path")
 
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
-exports.createPages = async ({ actions }) => {
+exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
+  const result = await graphql(`
+    query Pages {
+      pages: allMarkdownRemark {
+        nodes {
+          parent {
+            ... on File {
+              name
+            }
+          }
+          frontmatter {
+            categories
+          }
+          tableOfContents
+          html
+        }
+      }
+    }
+  `)
+
+  const pageTemplate = path.resolve(`src/templates/page.js`)
+  for (const page of result.data.pages.nodes) {
+    createPage({
+      path: `/${page.parent.name}`,
+      component: pageTemplate,
+      context: {
+        title: page.parent.name,
+        categories: page.frontmatter.categories
+          ? page.frontmatter.categories.split(", ")
+          : [],
+        tableOfContents: page.tableOfContents,
+        html: page.html,
+      },
+    })
+  }
 }
