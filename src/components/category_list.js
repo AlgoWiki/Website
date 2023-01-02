39152c27b7a55a7477d5c0bbc394f0844dafe1bd
Bjarki Ágúst Guðmundsson <bjarki.agust@gmail.com>
2023-01-02 23:32:22 +0100

    Style category list

--- src/components/category_list.js
+++ src/components/category_list.js
@@ -2,10 +2,15 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 const CategoryList = ({ categories }) => (
-  <ul>
+  <ul className="-mt-6 mb-6 flex">
     {categories.map(category => (
-      <li key={category}>
-        <Link to={`/Category/${category}`}>{category}</Link>
+      <li key={category} className="border-l first:border-l-0 first:-ml-2">
+        <Link
+          className="block px-2 text-sm text-gray-500"
+          to={`/Category/${category}`}
+        >
+          {category}
+        </Link>
       </li>
     ))}
   </ul>
