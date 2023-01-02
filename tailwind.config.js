a997e931660cc162f8527514c51b70b225832f89
Bjarki Ágúst Guðmundsson <bjarki.agust@gmail.com>
2023-01-02 22:24:40 +0100

    Style links

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,26 @@ module.exports = {
     "./src/templates/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      typography: theme => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.gray.800"),
+            a: {
+              color: theme("colors.gray.900"),
+              "text-underline-offset": "4px",
+              "font-weight": "400",
+              "&:hover": {
+                color: theme("colors.gray.600"),
+              },
+            },
+            strong: {
+              "font-weight": "500",
+            },
+          },
+        },
+      }),
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 }
