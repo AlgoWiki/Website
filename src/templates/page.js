6a9532767b4868b08e3d7f88be06877989c921d3
Bjarki Ágúst Guðmundsson <bjarki.agust@gmail.com>
2023-01-02 17:28:41 +0100

    Add TeX math support

--- src/templates/page.js
+++ src/templates/page.js
@@ -21,6 +21,28 @@ const Page = ({ pageContext }) => {
   )
 }
 
-export const Head = ({ pageContext }) => <Seo title={pageContext.title} />
+export const Head = ({ pageContext }) => (
+  <>
+    <Seo title={pageContext.title} />
+    <script>
+      {`
+        MathJax = {
+            tex: {
+                inlineMath: [['$', '$']]
+            },
+            svg: {
+                fontCache: 'global'
+            }
+        };
+      `}
+    </script>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+    <script
+      id="MathJax-script"
+      async
+      src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
+    ></script>
+  </>
+)
 
 export default Page
