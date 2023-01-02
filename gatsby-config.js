20a343db4448e7b4dcef1cdf4663cfe4a62e1671
Bjarki Ágúst Guðmundsson <bjarki.agust@gmail.com>
2023-01-02 17:15:39 +0100

    Update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `AlgoWiki`,
+    description: `AlgoWiki, a wiki dedicated to competitive programming.`,
+    author: `AlgoWiki`,
+    siteUrl: `https://wiki.algo.is`,
   },
   plugins: [
     {
